7acc9e550bc3e41d7e677837fa1c41d553c59722
Mauro Baladés <mauro.balades@tutanota.com>
2021-10-29 16:48:40 +0200

    changed category

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Topic :: Games/Entertainment",
+        "Topic :: Web",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     license="MIT",
